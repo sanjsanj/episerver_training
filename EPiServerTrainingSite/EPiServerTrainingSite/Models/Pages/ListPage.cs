9da0a33d8b219e0d1f4a5b1e5273e3342e425b2f
thisdotrob <robstevenson000@gmail.com>
2016-04-19 20:18:55 +0100

    Add a required number of items property to ListPage page type

--- EPiServerTrainingSite/EPiServerTrainingSite/Models/Pages/ListPage.cs
+++ EPiServerTrainingSite/EPiServerTrainingSite/Models/Pages/ListPage.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using EPiServer.Core;
-using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
-using EPiServer.SpecializedProperties;
 
 namespace EPiServerTrainingSite.Models.Pages
 {
     [ContentType(DisplayName = "ListPage", GUID = "292f929c-5313-4599-aa12-5f335edd21b5", Description = "")]
     public class ListPage : PageData
     {
-
+        [Required]
+        [Range(0, 10, ErrorMessage = "Select a value between 0 and 10")]
+        public virtual int NumberOfItems { get; set; }
     }
 }
