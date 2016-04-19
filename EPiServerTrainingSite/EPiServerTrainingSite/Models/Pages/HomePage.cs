4b863c12695c87f725d835ea62a022d88bda7381
thisdotrob <robstevenson000@gmail.com>
2016-04-19 19:23:13 +0100

    Add HomePage pagetype

---
+++ EPiServerTrainingSite/EPiServerTrainingSite/Models/Pages/HomePage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EPiServerTrainingSite.Models.Pages
+{
+    [ContentType(DisplayName = "HomePage", GUID = "2365453b-1b19-4b22-9215-f2c5a217ba07", Description = "")]
+    public class HomePage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
