a43eb44e01d0d05f6c929549cbc9af13897acecf
thisdotrob <robstevenson000@gmail.com>
2016-04-19 19:53:03 +0100

    Add LandingPage page type

---
+++ EPiServerTrainingSite/EPiServerTrainingSite/Models/Pages/LandingPage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EPiServerTrainingSite.Models.Pages
+{
+    [ContentType(DisplayName = "LandingPage", GUID = "1faca2fa-5456-46e4-b55a-12549949f4b4", Description = "")]
+    public class LandingPage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main content area",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual ContentArea MainContentArea { get; set; }
+
+    }
+}
