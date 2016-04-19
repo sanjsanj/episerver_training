f83176e121350ea25045be75cdc26de7d0671417
thisdotrob <robstevenson000@gmail.com>
2016-04-19 20:04:31 +0100

    Add ListPage page type

---
+++ EPiServerTrainingSite/EPiServerTrainingSite/Models/Pages/ListPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EPiServerTrainingSite.Models.Pages
+{
+    [ContentType(DisplayName = "ListPage", GUID = "292f929c-5313-4599-aa12-5f335edd21b5", Description = "")]
+    public class ListPage : PageData
+    {
+
+    }
+}
