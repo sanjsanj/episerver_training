cb64c201787ebaa6a0d1c7c8ab21bebea0a1ca5e
thisdotrob <robstevenson000@gmail.com>
2016-04-19 19:50:12 +0100

    Add HeroBlock block

---
+++ EPiServerTrainingSite/EPiServerTrainingSite/Models/Blocks/HeroBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EPiServerTrainingSite.Models.Blocks
+{
+    [ContentType(DisplayName = "HeroBlock", GUID = "0a2f6bc6-f161-4c60-8136-b511e7a0e05e", Description = "")]
+    public class HeroBlock : BlockData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Hero block",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string Name { get; set; }
+
+    }
+}
