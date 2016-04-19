ab2c3e2e043ed144a5649a692dac32e0d9ea370f
thisdotrob <robstevenson000@gmail.com>
2016-04-19 19:26:29 +0100

    Add HomePage Controller

---
+++ EPiServerTrainingSite/EPiServerTrainingSite/Controllers/HomePageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+using EPiServerTrainingSite.Models.Pages;
+
+namespace EPiServerTrainingSite.Controllers
+{
+    public class HomePageController : PageController<HomePage>
+    {
+        public ActionResult Index(HomePage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
